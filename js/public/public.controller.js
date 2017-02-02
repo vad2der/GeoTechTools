203bedc21b7f5b38e39d09247e4e4d6191c1cf0b
vad2der <vad.deryabin@gmail.com>
2017-02-01 18:37:18 -0700

    warning alert when IE, right sidenav

--- js/public/public.controller.js
+++ js/public/public.controller.js
@@ -4,13 +4,73 @@
 angular.module('public')
 .controller('MenuController', MenuController);
 
-MenuController.$inject = ['$scope', '$mdSidenav'];
-function MenuController($scope, $mdSidenav) {
-	
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
+MenuController.$inject = ['$scope', '$timeout', '$mdSidenav'];
+function MenuController($scope, $timeout, $mdSidenav) {
+	let ctrl = this;
+	ctrl.loggedin = false;
+	ctrl.fullName = null;
+  	$scope.toggleSidenav = function(menuId) {
+    	$mdSidenav(menuId).toggle();
+  	};
 
-	
-}
+    $scope.toggleLeft = buildDelayedToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    $scope.isOpenRight = function(){
+      return $mdSidenav('right').isOpen();
+    };
+
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      console.log("!!!");
+      $mdSidenav('right').close()
+        .then(function () {
+          //$log.debug("right is closed");
+        });
+    };
+    /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildDelayedToggler(navID) {
+      return debounce(function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            //$log.debug("toggle " + navID + " is done");
+          });
+      }, 200);
+    }
+
+    function buildToggler(navID) {
+      return function() {
+        // Component lookup should always be available since we are not using `ng-if`
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            //$log.debug("toggle " + navID + " is done");
+          });
+      };
+    }
+};
+
 })(window);
--- js/utilities.module.js
+++ js/utilities.module.js
@@ -5,7 +5,16 @@ angular.module('utilities', ['public', 'ngMdIcons'])
 .config(config)
 .controller('AppCtrl', function() {
   this.currentNavItem = "Home";
-});
+})
+.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('right').close()
+        .then(function () {
+          $log.debug("close RIGHT is done");
+        });
+    };
+  });
 
 config.$inject = ['$urlRouterProvider', '$locationProvider'];
 function config($urlRouterProvider, $locationProvider) {
